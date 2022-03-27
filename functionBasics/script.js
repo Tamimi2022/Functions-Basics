a2182b4686c7281a609788dcfe804e1f84629438
Farid Attamimi <97972332+Tamimi2022@users.noreply.github.com>
2022-03-27 18:23:11 +0200

    Add files via upload

---
+++ functionBasics/script.js
@@ -0,0 +1,29 @@
+const pizza = function() {
+    console.log("Make a flour");
+    console.log("add yeast");
+    console.log("Add salt");
+    console.log("Add water");
+    console.log("Mix with olive");
+};
+// Answer no.7 No, we need to call a function 
+
+// Answer no.8
+pizza();
+pizza();
+pizza(); // Answer no.9
+
+const sushi = function() {
+    console.log("Prep the Rice & Nori");
+    console.log("Spread the Rice on the Nori");
+    console.log("Add Fillings to the Bottom");
+    console.log("Roll the Sushi");
+    console.log("Cut the Sushi");
+};
+sushi();
+
+// Last Part
+sushi();
+sushi();
+sushi();
+pizza();
+pizza();
